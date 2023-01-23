a4f913c4e3f69ceff32b43ed3e0ea89d902634f0
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-01-23 02:36:29 -0300

    styles

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,9 +1,14 @@
 import { Link } from "react-router-dom";
+
 import s from "./NavBarStyles.module.css";
+
 function NavBar() {
   return (
     <>
       <nav className={`${s.nav}`}>
+        <div className={`${s.bg}`}></div>
+        <div className={`${s.capa}`}></div>
+
         <ul className={`${s.ul}`}>
           <li className={`${s.li}`}>
             <Link to="/home" className={`${s.link}`}>
