7c93bdbf2db12a701a5bd4ff4f1489c247d54289
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-02-05 23:08:32 -0300

    useAxios optimizated

--- client/src/hooks/useAxios.js
+++ client/src/hooks/useAxios.js
@@ -2,29 +2,33 @@ import axios from "../services/axiosBaseUrl";
 import { useEffect, useState } from "react";
 
 export function useAxios(url) {
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(false);
+  const [state, setState] = useState({
+    isLoaded: null,
+    data: null,
+    error: false,
+  });
 
-  useEffect(
-    () =>
-      (async () => {
-        try {
-          let { data } = await axios.get(url);
-          // if (!data) throw Error({ error: true, status: data.status }); // probar si anda así
-          setData(data);
-          setIsLoaded(true);
-        } catch (error) {
-          setError(error);
-          setIsLoaded(true);
-        }
-        return () => {
-          setData(null);
-          setIsLoaded(false);
-          setError(false);
-        };
-      })(),
-    [url]
-  );
-  return [isLoaded, data, error];
+  useEffect(() => {
+    (async () => {
+      try {
+        let { data } = await axios.get(url);
+        // if (!data) throw Error({ error: true, status: data.status }); // probar si anda así
+        setState((prev) => {
+          return { ...prev, data, isLoaded: true };
+        });
+      } catch (error) {
+        setState((prev) => {
+          return { ...prev, isLoaded: true, error };
+        });
+      }
+    })();
+    return () => {
+      setState({
+        isLoaded: null,
+        data: null,
+        error: false,
+      });
+    };
+  }, [url]);
+  return [state.isLoaded, state.data, state.error];
 }
