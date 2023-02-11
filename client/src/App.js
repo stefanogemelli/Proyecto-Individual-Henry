20f6272a6f07e7b8968d01d5e7b3b4c8bcf3a838
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-02-10 21:26:57 -0300

    lazyLoading

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
-import { Switch, Route, useLocation } from "react-router-dom";
-
-import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
-import About from "./pages/About/About";
-import Activities from "./pages/Activities/Activities";
-import Detail from "./pages/Detail/Detail";
-import Error404 from "./pages/Error404/Error404";
-import Home from "./pages/Home/Home";
-import Landing from "./pages/Landing/Landing";
-import UpdateActivity from "./pages/UpdateActivity/UpdateActivity";
+import { Suspense, lazy } from "react";
+import { Switch, Route, useLocation } from "react-router-dom";
+import "./App.css";
+const Home = lazy(() => import("./pages/Home/Home"));
+const Landing = lazy(() => import("./pages/Landing/Landing"));
+const UpdateActivity = lazy(() =>
+  import("./pages/UpdateActivity/UpdateActivity")
+);
+const About = lazy(() => import("./pages/About/About"));
+const Activities = lazy(() => import("./pages/Activities/Activities"));
+const Detail = lazy(() => import("./pages/Detail/Detail"));
+const Error404 = lazy(() => import("./pages/Error404/Error404"));
 
 function App() {
   const { pathname } = useLocation();
@@ -24,19 +26,21 @@ function App() {
         </>
       )}
 
-      <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/home" component={Home} />
-        <Route exact path="/detail/:idCountry" component={Detail} />
-        <Route exact path="/activities" component={Activities} />
-        <Route
-          exact
-          path="/activities/update/:idActivity"
-          component={UpdateActivity}
-        />
-        <Route exact path="/about" component={About} />
-        <Route path="/*" component={Error404} />
-      </Switch>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/detail/:idCountry" component={Detail} />
+          <Route exact path="/activities" component={Activities} />
+          <Route
+            exact
+            path="/activities/update/:idActivity"
+            component={UpdateActivity}
+          />
+          <Route exact path="/about" component={About} />
+          <Route path="/*" component={Error404} />
+        </Switch>
+      </Suspense>
     </div>
   );
 }
